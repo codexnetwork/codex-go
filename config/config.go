fe27ff638ae87f5fc79e38af8b74eceb10e44d13
fanyang <fanyang.coder@gmail.com>
2019-01-23 17:53:56 +0800

    add private keys for signer in config

--- config/config.go
+++ config/config.go
@@ -10,13 +10,15 @@ type configDatas struct {
 	ChainID string           `json:"chainid"`
 	URL     string           `json:"url"`
 	Keys    []accountKeyData `json:"keys"`
+	PriKeys []string         `json:"pri"`
 }
 
 // Config config to force-go
 type Config struct {
 	ChainID eos.SHA256Bytes
 	URL     string
-	keys    map[string]accountKey
+	Keys    map[string]accountKey
+	Prikeys []ecc.PrivateKey
 }
 
 // Data config data for force-go
@@ -28,7 +30,7 @@ var Cfg Config
 // Parse parse cfg from data
 func (c *Config) Parse(data *configDatas) {
 	// keys
-	c.keys = make(map[string]accountKey, 64)
+	c.Keys = make(map[string]accountKey, 64)
 	for _, k := range data.Keys {
 		pk, err := ecc.NewPrivateKey(k.PriKey)
 		if err != nil {
@@ -40,8 +42,18 @@ func (c *Config) Parse(data *configDatas) {
 		}
 		n.PubKey = n.PriKey.PublicKey()
 
-		c.keys[k.PriKey] = n
+		c.Keys[k.PriKey] = n
 		seelog.Debugf("load account key %s -> %s", n.Name, n.PubKey)
+	}
+
+	c.Prikeys = make([]ecc.PrivateKey, 0, len(data.PriKeys)+64)
+	for _, k := range data.PriKeys {
+		pk, err := ecc.NewPrivateKey(k)
+		if err != nil {
+			panic(err)
+		}
+		c.Prikeys = append(c.Prikeys, *pk)
+		seelog.Debugf("load key %s", pk.PublicKey())
 	}
 
 	//chainID
