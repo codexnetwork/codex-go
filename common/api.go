a2696ea05c21122d2c4b0381f84a79ecf8ac34b0
fanyang <fanyang.coder@gmail.com>
2019-01-23 17:04:47 +0800

    use a wapper to eos.API

--- common/api.go
+++ common/api.go
@@ -2,10 +2,20 @@ package common
 
 import (
 	eos "github.com/eosforce/goeosforce"
+	"github.com/fanyang1988/force-go/config"
 )
 
+// API client api to forceio chain
+type API struct {
+	*eos.API
+	Cfg config.Config
+}
+
 // NewAPI create api to apiURL
-func NewAPI(apiURL string) *eos.API {
+func NewAPI(apiURL string) *API {
 	api := eos.New(apiURL)
-	return api
+	return &API{
+		api,
+		config.Cfg,
+	}
 }
