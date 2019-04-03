352e7fbbc9fb9a592508f3d1fbb6733f1ff272d9
fanyang <fanyang.coder@gmail.com>
2019-04-03 15:24:24 +0800

    change configDatas to ConfigData

--- config/config.go
+++ config/config.go
@@ -7,7 +7,8 @@ import (
 	"github.com/pkg/errors"
 )
 
-type configDatas struct {
+// ConfigData JSON struct define to config
+type ConfigData struct {
 	ChainID string           `json:"chainid"`
 	URL     string           `json:"url"`
 	Keys    []accountKeyData `json:"keys"`
@@ -25,7 +26,7 @@ type Config struct {
 
 // LoadCfgFromFile load cfg from file
 func LoadCfgFromFile(path string) (*Config, error) {
-	var data configDatas
+	var data ConfigData
 	err := LoadJSONFile(path, &data)
 	if err != nil {
 		return nil, errors.Wrapf(err, "load %s err", path)
@@ -44,7 +45,7 @@ func LoadCfgFromFile(path string) (*Config, error) {
 }
 
 // Parse parse cfg from data
-func (c *Config) Parse(data *configDatas) error {
+func (c *Config) Parse(data *ConfigData) error {
 	// keys
 	c.Keys = make(map[string]accountKey, 64)
 	for _, k := range data.Keys {
