810b7a535b44b2309a1b62f4de5c72b09797b627
fanyang <fanyang.coder@gmail.com>
2019-04-28 20:00:38 +0800

    add chain typ

--- client.go
+++ client.go
@@ -5,13 +5,28 @@ import (
 	"github.com/fanyang1988/force-go/config"
 )
 
+type ClientType uint8
+
+const (
+	ClientTypeNil = ClientType(iota)
+	FORCEIO
+	EOSForce
+	Codex
+	EOSIO
+	ENU     // no support now
+	BOS     // no support now
+	TLOS    // no support now
+	MEETONE // no support now
+)
+
 // Client client to forceio chain
 type Client struct {
 	api *common.API
+	typ ClientType
 }
 
 // NewClient create client by config data
-func NewClient(cfg *config.Config) (*Client, error) {
+func NewClient(typ ClientType, cfg *config.Config) (*Client, error) {
 	api, err := common.NewAPI(cfg)
 	if err != nil {
 		return nil, err
@@ -19,14 +34,15 @@ func NewClient(cfg *config.Config) (*Client, error) {
 
 	return &Client{
 		api: api,
+		typ: typ,
 	}, nil
 }
 
 // NewClientFromFile create client by config file
-func NewClientFromFile(path string) (*Client, error) {
+func NewClientFromFile(typ ClientType, path string) (*Client, error) {
 	cfg, err := config.LoadCfgFromFile(path)
 	if err != nil {
 		return nil, err
 	}
-	return NewClient(cfg)
+	return NewClient(typ, cfg)
 }
